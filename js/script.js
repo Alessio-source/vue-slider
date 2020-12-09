5cb39490724f89b3cffc40a1bf408f24d9dff8e2
Alessio Pancia <alessiop2801@gmail.com>
2020-12-09 15:23:09 +0100

    css html e base script

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    imageIndex: 0,
+    images: [
+      "https://www.jovittorio.com/wp-content/uploads/2018/01/fotografare-paesaggi-1.jpg",
+      "https://www.jovittorio.com/wp-content/uploads/2018/02/v%C3%B6lkermarkt_stausee.jpg",
+      "https://www.altraeta.it/over50/wp-content/uploads/2018/11/dolomites-2580866_1920-1024x638.jpg",
+      "https://www.marathonclubtrento.com/wp/wp-content/uploads/2018/07/riva-hm-2017-0-1024x638.jpg"
+    ]
+  }
+})
